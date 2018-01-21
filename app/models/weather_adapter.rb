14311b0477f92e760aa4ce3d2adc620cb7d6b8ea
Ariel Herman <aeh.herman@gmail.com>
2018-01-21 14:54:23 -0500

    add application.yml file (gitignored), update API keys

--- app/models/weather_adapter.rb
+++ app/models/weather_adapter.rb
@@ -1,7 +1,9 @@
 class WeatherAdapter < ApplicationRecord
   require 'open-uri'
+  require 'nokogiri'
+
   def self.local_xml(location)
-      url1  = "http://api.worldweatheronline.com/premium/v1/weather.ashx?q=#{location}&format=xml&num_of_days=5&key=#{ENV["weather_key"]}"
+      url1  = "http://api.worldweatheronline.com/premium/v1/weather.ashx?q=#{location}&format=xml&num_of_days=5&key=#{ENV["WEATHER_KEY"]}"
       Nokogiri::XML(open(url1))
   end
 
